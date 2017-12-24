73cdbd9a15543874c9fd9bb110a72ca332f44fc9
haykam821 <haykam821@users.noreply.github.com>
2017-12-24 18:47:33 -0500

    Changes

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-exports = function(players) {
-  console.log(players);
+exports.sort = function(players) {
+
+  for (i in Object.values(players)) {
+    //layers[Object.values(players)][i].score
+  }
+
+  return //sorted
+  console.log(players)
 }
