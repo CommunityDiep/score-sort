1e1d4b690db50cf9c4ee919338710b730700c369
haykam821 <haykam821@users.noreply.github.com>
2017-12-25 13:20:00 -0500

    Don't console.log; fix percentage if top score is 0

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ function sort (players) {
 		sorted.push([]);
 
 		let num = numeral(stripped.scores[i]).format('0.[0]a');
+		let maxscore = stripped.scores[0] == 0 ? 1 : stripped.scores[0];
 
 		sorted[i].push(`${stripped.names[i]} - ${num}`);
-		sorted[i].push(stripped.scores[i] / stripped.scores[0]);
+		sorted[i].push(stripped.scores[i] / maxscore);
 	}
-	console.log(sorted)
 	return players
 }
 
