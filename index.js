b2138b62b55dc62450bd15de9165e106d4b2a575
Infrared Ninja <zacharylodgevproxy@gmail.com>
2017-12-24 16:13:50 -0800

    Formatting

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-exports.sort = function(players) {
-
-  for (i in Object.values(players)) {
-    //layers[Object.values(players)][i].score
-  }
-
-  return //sorted
-  console.log(players)
+function sort (players) {
+	for (let i in Object.values(players)) {
+		// layers[Object.values(players)][i].score
+	}
+	console.log(players);
+	return // sorted;
 }
+
+module.exports.sort = sort;
