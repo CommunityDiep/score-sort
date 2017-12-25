847268325d9859e0c933fe114ade306ca7961b61
Infrared Ninja <zacharylodgevproxy@gmail.com>
2017-12-24 19:20:27 -0800

    Formatting

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
-function highestSort(a,b) {
-    return a - b;
-}
+const numeral = require('numeral');
 
-var numeral = require('numeral');
+function sort (players) {
+	let sorted = [];
+	let stripped = {
+		scores: [],
+		names: []
+	};
 
-exports.sort = function(players) {
-  var sorted = [];
-
-  var stripped = {
-  	scores: [],
-    names: []
-  };
-
-  for (var i in Object.values(players)) {
-    stripped.names.push(Object.values(players)[i].name);
-    stripped.scores.push(Object.values(players)[i].score);
-
+	for (var i in Object.values(players)) {
+		stripped.names.push(Object.values(players)[i].name);
+		stripped.scores.push(Object.values(players)[i].score);
 		sorted.push([]);
 
-		var num = numeral(stripped.scores[i]).format('0.[0]a');
+		let num = numeral(stripped.scores[i]).format('0.[0]a');
 
 		sorted[i].push(`${stripped.names[i]} - ${num}`);
 		sorted[i].push(stripped.scores[i] / stripped.scores[0]);
 	}
-
 	console.log(sorted)
-
-  return players
+	return players
 }
+
+module.exports.sort = sort;
