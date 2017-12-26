bce28fcc5894f17e8bc04b7e6027167d8a3488af
Infrared Ninja <zacharylodgevproxy@gmail.com>
2017-12-25 21:24:24 -0800

    Formatting and best practices

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const numeral = require("numeral");
+const numeral = require('numeral');
 
-let sort = (p, depth)=>{
-    let arr = Object.values(p).sort( (a,b)=>b.score-a.score);
-    depth = depth == undefined ? 1 : depth;
-
-    return arr.map(y=> ({...y, percentage: y.score/(arr[0].score == 0 ? 1 : arr[0].score)}), [1]).map(j=>[`${j.name} - ${numeral(j.score).format('0.[' + '0'.repeat(depth) + ']a')}`, j.percentage])
-}
+const sort = (players, depth = 1) => {
+	let arr = Object.values(players).sort((a, b) => b.score - a.score);
+	return arr.map(
+		elem =>
+			({
+				...elem,
+				percentage: elem.score / (arr[0].score === 0 ? 1 : arr[0].score),
+			})
+	).map(
+		elem =>
+			[
+				`${elem.name} - ${numeral(elem.score).format('0.[' + '0'.repeat(depth) + ']a')}`,
+				elem.percentage,
+			]
+	);
+};
 
 module.exports.sort = sort;
