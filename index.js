92305d797e4bd440c0cd993afd59e8f5efdeec6d
haykam821 <haykam821@users.noreply.github.com>
2017-12-26 00:12:54 -0500

    Added depth
    This should work

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const numeral = require("numeral");
 
-let sort = (p)=>{
-    let arr = Object.values(p).sort( (a,b)=>b.score-a.score)
-    return arr.map(y=> ({...y, percentage: y.score/(arr[0].score == 0 ? 1 : arr[0].score)}), [1]).map(j=>[`${j.name} - ${numeral(j.score).format('0.[0]a')}`, j.percentage])
+let sort = (p, depth)=>{
+    let arr = Object.values(p).sort( (a,b)=>b.score-a.score);
+    depth = depth == undefined ? 1 : depth;
+
+    return arr.map(y=> ({...y, percentage: y.score/(arr[0].score == 0 ? 1 : arr[0].score)}), [1]).map(j=>[`${j.name} - ${numeral(j.score).format('0.[' + '0'.repeat(depth) + ']a')}`, j.percentage])
 }
 
 module.exports.sort = sort;
