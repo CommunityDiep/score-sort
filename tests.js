061a74f98e8141e6a7e01076e4b519ccc70704a7
haykam821 <haykam821@users.noreply.github.com>
2018-01-21 15:06:57 -0500

    Test functionaliting

--- tests.js
+++ tests.js
@@ -4,6 +4,30 @@ const assert = chai.assert;
 const sort = require('./index.js').sort;
 
 describe('sort', function() {
-  it('should return an array');
-  it('should have as many arrays in the array as keys in the first argument');
+  it('should return an array', function() {
+    assert.typeOf(Array.issort({test:{score:1,name:"hi"}}), 'array');
+  });
+
+  it('should have as many arrays in the array as keys in the first argument', function() {
+    let lengthIsOne = sort({
+      test: {
+        score: 1337,
+        name: "This is a string"
+      }
+    });
+
+    let lengthIsTwo = sort({
+      test: {
+        score: 1337,
+        name: "This is a string"
+      },
+      test1: {
+        score: 9999,
+        name: "This is another string"
+      }
+    });
+
+    assert.equal(1, lengthIsOne.length);
+    assert.equal(2, lengthIsTwo.length);
+  });
 });
