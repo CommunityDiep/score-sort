09f03168ceb0fd4ddb0fd0d3abc8b265e29dc8d8
haykam821 <haykam821@users.noreply.github.com>
2018-01-21 14:59:49 -0500

    sort is the function that it has

--- tests.js
+++ tests.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const scoreSort = require('./index.js');
+const sort = require('./index.js').sort;
 
 describe('sort', function() {
   it('should return an array');
