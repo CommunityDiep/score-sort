e5e4f21424351bd2bc677450c597d0484cfb8749
haykam821 <haykam821@users.noreply.github.com>
2018-01-21 14:55:39 -0500

    Require the main file

--- tests.js
+++ tests.js
@@ -1,6 +1,8 @@
 const chai = require('chai');
 const assert = chai.assert;
 
+const scoreSort = require('./index.js');
+
 describe('sort', function() {
   it('should return an array');
   it('should have as many arrays in the array as keys in the first argument');
