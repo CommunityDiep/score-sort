339b84abaec02b39035313f736f97e66e9ef6336
haykam821 <haykam821@users.noreply.github.com>
2018-01-21 14:54:37 -0500

    Start writing tests?

--- tests.js
+++ tests.js
@@ -1,2 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
+
+describe('sort', function() {
+  it('should return an array');
+  it('should have as many arrays in the array as keys in the first argument');
+});
